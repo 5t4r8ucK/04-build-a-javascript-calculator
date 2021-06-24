795bd42fd2216d06c5dc556828b243f0e380cabe
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-06-24 22:04:52 +0200

    Moved App into components folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
+// CSS
 import './index.css';
-import App from './App';
+// Components
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
