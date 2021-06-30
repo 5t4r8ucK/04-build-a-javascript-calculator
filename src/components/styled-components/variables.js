cfdcc5ce86c45787b1b506128e23d37752e419b1
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-06-30 16:54:52 +0200

    Created variables for colors, fonts, and sizes.

---
+++ src/components/styled-components/variables.js
@@ -0,0 +1,27 @@
+
+// Fonts
+export const font_stack_digital = '"Digital dream", monospace';
+export const font_stack_sans_serif = '"Jura", sans-serif';
+
+// Colors
+export const black_0 = 'rgba(0 ,0 ,0, 0)';
+export const black_10 = 'rgba(0 ,0 ,0, 0.1)';
+export const black_50 = 'rgba(0 ,0 ,0, 0.5)';
+export const black_100 = 'rgba(0 ,0 ,0, 1)';
+
+export const white_0 = 'rgba(255, 255, 255, 0)';
+export const white_50 = 'rgba(255, 255, 255, 0.5)';
+export const white_100 = 'rgba(255, 255, 255, 1)';
+
+export const cyan_100 = 'rgba(155, 254, 254, 1)';
+export const cyan_dark_100 = 'rgba(19, 147, 180, 1)';
+export const blue_dark_100 = 'rgba(5, 15, 27, 1)';
+export const gray_100 = 'rgba(134, 150, 174, 1)';
+export const orange_100 = 'rgba(174, 106, 35, 1)';
+// Measurements
+export const gap_width = '1.75rem'; // 28 px
+export const hairline_width = '0.0625rem'; // 1 px
+export const border_width = '0.125rem';// 2 px
+export const button_border_radius = '0.3125rem'; // 5 px
+export const calculator_border_radius = '0.625rem';//10 px
+export const column_width = '4rem'; // 64 px
