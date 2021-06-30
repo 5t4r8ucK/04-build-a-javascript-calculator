5f7e665cd99cc4c632ccbe276a28c387dd16b90a
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-06-30 16:55:22 +0200

    Created a wrapper component in which the calculator will be housed.

---
+++ src/components/styled-components/wrapper.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import * as variables from './variables.js';
+
+const Wrapper = styled.section`
+  // Positioning
+  // Display and Box Model
+  align-items: center;
+  background-color: ${variables.black_50};
+  display: flex;
+  min-height: 100vh;
+  justify-content: center;
+  // Typography
+  // Other
+`
+export default Wrapper;
