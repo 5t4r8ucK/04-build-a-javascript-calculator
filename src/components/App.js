ce0a118493b11f9297117e984125efc1d75e87a2
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-06-30 16:59:50 +0200

    Imported all the components into the main App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,45 @@
-import './App.css';
+// React
+import React from 'react';
 // Styled Components
-import Button from './button.js';
+import Wrapper from './styled-components/wrapper.js';
+import Calculator from './styled-components/calculator.js';
+import Name from './styled-components/name.js';
+import Display from './styled-components/display.js';
+import Numbers from './styled-components/numbers.js';
+import KeyPad from './styled-components/keyPad.js';
+import Button from './styled-components/button.js';
 
 function App() {
   return (
-    <div className="App">
-      <Button>Test Button</Button>
-    </div>
+    <Wrapper id='wrapper'>
+      <Calculator id='calculator'>
+        <Name id='name'>Calculator</Name>
+        <Display id='display'>
+          <Numbers className='numbers'>
+            1234567890
+          </Numbers>
+        </Display>
+        <KeyPad id='keypad'>
+          <Button id='clear' color="orange">C</Button>
+          <Button id='divide' color="gray">÷</Button>
+          <Button id='multiply' color="gray">×</Button>
+          <Button id='subtract' color="gray">-</Button>
+          <Button id='seven'>7</Button>
+          <Button id='eight'>8</Button>
+          <Button id='nine'>9</Button>
+          <Button id='add' color="gray">+</Button>
+          <Button id='four'>4</Button>
+          <Button id='five'>5</Button>
+          <Button id='six'>6</Button>
+          <Button id='equals' color="gray">=</Button>
+          <Button id='one'>1</Button>
+          <Button id='two'>2</Button>
+          <Button id='three'>3</Button>
+          <Button id='zero'>0</Button>
+          <Button id='decimal'>•</Button>
+        </KeyPad>
+      </Calculator>
+    </Wrapper>
   );
 }
 
