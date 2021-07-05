f7a6f10f74fb0e71231cdb0107610acbe4f58299
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-05 21:18:32 +0200

    Added an array of button objects containing their properties.

---
+++ src/assets/buttonContent.js
@@ -0,0 +1,89 @@
+const buttonContent = [
+  {
+    id: 'add',
+    color: 'gray',
+    symbol: '+'
+  },
+  {
+    id: 'clear',
+    color: 'orange',
+    symbol: 'C'
+  },
+  {
+    id: 'decimal',
+    color: '',
+    symbol: '.'
+  },
+  {
+    id: 'divide',
+    color: 'gray',
+    symbol: '÷'
+  },
+  {
+    id: 'eight',
+    color: '',
+    symbol: '8'
+  },
+  {
+    id: 'equals',
+    color: 'gray',
+    symbol: '='
+  },
+  {
+    id: 'five',
+    color: '',
+    symbol: '5'
+  },
+  {
+    id: 'four',
+    color: '',
+    symbol: '4'
+  },
+  {
+    id: 'multiply',
+    color: 'gray',
+    symbol: '×'
+  },
+  {
+    id: 'nine',
+    color: '',
+    symbol: '9'
+  },
+  {
+    id: 'one',
+    color: '',
+    symbol: '1'
+  },
+  {
+    id: 'seven',
+    color: '',
+    symbol: '7'
+  },
+  {
+    id: 'subtract',
+    color: 'gray',
+    symbol: '-'
+  },
+  {
+    id: 'six',
+    color: '',
+    symbol: '6'
+  },
+  {
+    id: 'three',
+    color: '',
+    symbol: '3'
+  },
+  {
+    id: 'two',
+    color: '',
+    symbol: '2'
+  },
+  {
+    id: 'zero',
+    color: '',
+    symbol: '0'
+  },
+];
+
+export default buttonContent;
