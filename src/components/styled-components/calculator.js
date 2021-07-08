6c5908041c67713c6b4fdb9ae4101c13e518e3b0
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-07-08 18:33:16 +0200

    Changed the width to compensate for new borders. Changed the colors slightly.

--- src/components/styled-components/calculator.js
+++ src/components/styled-components/calculator.js
@@ -5,15 +5,15 @@ const Calculator = styled.div`
   // Positioning
   z-index: 0;
   // Display and Box Model
-  background-color: ${variables.gray_100};
+  background-color: ${variables.silver_100};
   border-radius: ${variables.calculator_border_radius};
   border-top-color: ${variables.white_100};
   border-left-color: ${variables.white_50};
-  border-bottom-color: ${variables.black_100};
-  border-right-color: ${variables.black_50};
+  border-bottom-color: ${variables.black_50};
+  border-right-color: ${variables.white_50};
   border-style: solid;
   border-width: ${variables.border_width};
-  box-shadow: 0 1.25rem 0 ${variables.black_100};
+  box-shadow: 0 1.25rem 0 ${variables.silver_shadow};
   display: grid;
   gap: ${variables.gap_width};
   grid-template-areas:
@@ -23,7 +23,7 @@ const Calculator = styled.div`
   height: auto;
   justify-content: center;
   padding: ${variables.column_width};
-  width: calc((${variables.column_width} * 4) + (${variables.gap_width} * 3) + (${variables.column_width} * 2) + (${variables.border_width} * 2)); //4 buttons + 3 gaps + 2 padding + 2 border
+  width: calc((${variables.column_width} * 4) + (${variables.gap_width} * 3) + (${variables.column_width} * 2) + (${variables.border_width} * 4)); //4 buttons + 3 gaps + 2 padding + 4 border
   // Typography
   // Other
   //transform: rotateX(20deg);
